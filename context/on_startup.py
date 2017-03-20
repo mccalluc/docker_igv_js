71e1f95599247421be266747fbcfce6d7462cc7d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-03-20 17:32:12 -0400

    Check point: browser loads. 404s for hello_world.txt

--- context/on_startup.py
+++ context/on_startup.py
@@ -2,46 +2,58 @@ from os import listdir
 import json
 import SimpleHTTPServer
 import SocketServer
+import re
+
+def as_track(file):
+    return {
+        "name": re.sub(r'.*\/', '', file),
+        "url": file,
+        "format": re.sub(r'.*\.', '', file)
+        # TODO: indexURL if BAM
+    }
 
 def populate_data_directory():
-    # files.txt is for testing rather than production use.
+    # files.txt is really for testing rather than production use.
     files = '\n'.join(listdir('data'))
-    with open('data/files.txt', 'w') as file:
-        file.write(files)
+    with open('data/files.txt', 'w') as list_file:
+        list_file.write(files)
 
-    # config.json is actually used by IGV.js.
-    options = {
-        "palette": ["#00A0B0", "#6A4A3C", "#CC333F", "#EB6841"],
-        "locus": "7:55,085,725-55,276,031",
+    # options.json is actually used by IGV.js.
+    # TODO: How does Refinery communicate the genome assembly?
+    assembly = "hg19"
+    url_base = "https://s3.amazonaws.com/data.cloud.refinery-platform.org/data/igv-reference/{}/".format(assembly)
 
-        "reference": {
-            "id": "hg19",
-            "fastaURL": "//igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta",
-            "cytobandURL": "//igv.broadinstitute.org/genomes/seq/b37/b37_cytoband.txt"
-        },
-
-        "trackDefaults": {
-            "bam": {
-                "coverageThreshold": 0.2,
-                "coverageQualityWeight": "true"
-            }
-        },
-
-        "tracks": [
-            {
-                "name": "Genes",
-                "url": "//igv.broadinstitute.org/annotations/hg19/genes/gencode.v18.collapsed.bed",
-                "index": "//igv.broadinstitute.org/annotations/hg19/genes/gencode.v18.collapsed.bed.idx",
-                "displayMode": "EXPANDED"
-            }
-        ]
+    tracks = [as_track(f) for f in listdir('data')]
+    tracks.append({
+        "name": "Genes",
+        "type": "annotation",
+        "format": "bed",
+        "sourceType": "file",
+        "url": url_base + "refGene.bed",
+        "indexURL": url_base + "refGene.bed.tbi",
+        "visibilityWindow": 300000000,
+        "displayMode": "EXPANDED"
+    })
+    reference = {
+        "fastaURL": url_base + assembly + ".fa",
+        "indexURL": url_base + assembly + ".fa.fai",
+        "cytobandURL": url_base + "cytoBand.txt",
     }
+
+    # TODO: Figure out the highest genome address and we'll set the window accordingly.
+    options = {
+        "reference": reference,
+        "tracks": tracks
+    }
+
     with open('data/options.json', 'w') as options_file:
         options_file.write(json.dumps(options))
+
 
 def start_server():
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     SocketServer.TCPServer(("", 80), Handler).serve_forever()
+
 
 if __name__ == '__main__':
     populate_data_directory()
