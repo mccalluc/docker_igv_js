b430c15fba3b3f317dfefacce9b1b90b1750d6ad
Scott Ouellette <scottx611x@gmail.com>
2017-04-11 14:52:27 -0400

    Edit `on_startup` to ingest a list of urls rather than files

--- context/on_startup.py
+++ context/on_startup.py
@@ -1,39 +1,26 @@
-from os import listdir
 import json
 import SimpleHTTPServer
 import SocketServer
-import re
 
-def as_track(file):
-    return {
-        "name": re.sub(r'.*\/', '', file),
-        "url": 'data/' + file,
-        "format": re.sub(r'.*\.', '', file)
-        # TODO: indexURL if BAM
-    }
 
-def populate_data_directory():
-    # files.txt is really for testing rather than production use.
-    files = '\n'.join(listdir('data'))
-    with open('data/files.txt', 'w') as list_file:
-        list_file.write(files)
+def populate_igv_configuration():
+    with open("data/input.json") as f:
+        config_data = json.loads(f.read())
 
-    # options.json is actually used by IGV.js.
-    # TODO: How does Refinery communicate the genome assembly?
+    tracks = []
     assembly = "hg19"
-    url_base = "https://s3.amazonaws.com/data.cloud.refinery-platform.org/data/igv-reference/{}/".format(assembly)
+    url_base = "https://s3.amazonaws.com/data.cloud.refinery-platform.org/data/igv-reference/{}/".format(
+        assembly
+    )
 
-    tracks = [as_track(f) for f in listdir('data') if f not in ["files.txt", "hello_world.txt"]]
-    tracks.append({
-        "name": "Genes",
-        "type": "annotation",
-        "format": "bed",
-        "sourceType": "file",
-        "url": url_base + "refGene.bed",
-        "indexURL": url_base + "refGene.bed.tbi",
-        "visibilityWindow": 300000000,
-        "displayMode": "EXPANDED"
-    })
+    for url in config_data["file_relationships"]:
+        tracks.append({
+            "name": url.split("/")[-1],
+            "sourceType": "file",
+            "url":  url,
+            # "indexURL": url_base + "refGene.bed.tbi",
+            "visibilityWindow": 300000000
+        })
     reference = {
         "fastaURL": url_base + assembly + ".fa",
         "indexURL": url_base + assembly + ".fa.fai",
@@ -48,12 +35,11 @@ def populate_data_directory():
     with open('data/options.json', 'w') as options_file:
         options_file.write(json.dumps(options))
 
-
 def start_server():
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     SocketServer.TCPServer(("", 80), Handler).serve_forever()
 
 
 if __name__ == '__main__':
-    populate_data_directory()
+    populate_igv_configuration()
     start_server()
