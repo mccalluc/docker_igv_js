298e0ac35c20d51fafc51b3c0a3f5eaf7b8075af
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-04-12 09:37:16 -0400

    Revert the build_run_test changes... instead source envvars into both contexts.

--- build_run_test.sh
+++ build_run_test.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# On travis, the script is sourced, so these will be available outside:
-OWNER=gehlenborglab
-NAME=docker_igv_js
-REPO=$OWNER/$NAME
+source define_repo.sh
 
-# For local development, sudo is not needed.
-[ -z "$TRAVIS" ] && DOCKER='docker' || DOCKER='sudo docker'
-
-$DOCKER pull $REPO
-$DOCKER build --tag $NAME \
+docker pull $REPO
+docker build --tag $NAME \
              --cache-from $REPO \
              context
 
@@ -19,7 +13,7 @@ cp -a data_fixture $DATA_DIR
 # The on_startup script writes to the shared volume,
 # so we copy first to avoid contamination of the fixture.
 
-$DOCKER run --detach \
+docker run --detach \
            --name $NAME \
            --publish 80 \
            --volume $DATA_DIR:/var/www/data \
