bd8b5d04ef23201e9992cdba87758d21eac9a596
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-04-12 09:17:47 -0400

    DRYer: we hit a name mismatch problem earlier.

--- build_run_test.sh
+++ build_run_test.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# On travis, the script is sourced, so these will be available outside:
 OWNER=gehlenborglab
 NAME=docker_igv_js
-docker pull $OWNER/$NAME
-docker build --tag $NAME \
-             --cache-from $OWNER/$NAME \
+REPO=$OWNER/$NAME
+
+# For local development, sudo is not needed.
+[ -z "$TRAVIS" ] && DOCKER='docker' || DOCKER='sudo docker'
+
+$DOCKER pull $REPO
+$DOCKER build --tag $NAME \
+             --cache-from $REPO \
              context
 
 DATA_DIR=/tmp/docker_igv_js_`date +"%Y-%m-%d_%H-%M-%S"`
@@ -13,7 +19,7 @@ cp -a data_fixture $DATA_DIR
 # The on_startup script writes to the shared volume,
 # so we copy first to avoid contamination of the fixture.
 
-docker run --detach \
+$DOCKER run --detach \
            --name $NAME \
            --publish 80 \
            --volume $DATA_DIR:/var/www/data \
