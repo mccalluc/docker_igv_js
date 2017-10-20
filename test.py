367514e25f6bdcace8a8196dddd52f080f2283bf
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-20 17:34:05 -0400

    Test that we get expected error response

--- test.py
+++ test.py
@@ -7,13 +7,9 @@ import sys
 
 class ContainerTest(unittest.TestCase):
     def setUp(self):
-        # self.suffix = os.environ['SUFFIX']
-        # self.stamp = os.environ['STAMP']
+        # This is inefficient, but that doesn't matter: it's fast enough.
         self.good_url = self.get_url(os.environ['GOOD_NAME'])
         self.bad_url = self.get_url(os.environ['BAD_NAME'])
-
-    def tearDown(self):
-        print('good url: ' + self.good_url)
 
     def get_url(self, name):
         command = "docker port {} | perl -pne 's/.*://'".format(name)
@@ -26,14 +22,22 @@ class ContainerTest(unittest.TestCase):
             time.sleep(1)
         self.fail('Server never came up: ' + name)
 
-    def test_home_page(self):
+    def test_good_home_page(self):
         response = requests.get(self.good_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertRegexpMatches(response.text, r'>IGV<')
+        self.assertEqual(200, response.status_code)
+        self.assertIn('>IGV<', response.text)
+
+    def test_bad_home_page(self):
+        response = requests.get(self.bad_url)
+        self.assertEqual(200, response.status_code)  # Not ideal, but ok for now.
+        self.assertIn(
+            'Unexpected 404 from https://s3.amazonaws.com/data.cloud.refinery-platform.org/data/igv-reference/hgFAKE/cytoBand.txt',
+            response.text
+        )
 
     def test_data_directory(self):
         response = requests.get(self.good_url + 'data/input.json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(ContainerTest)
