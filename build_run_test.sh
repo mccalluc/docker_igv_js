84af4f58b3ef52e21ee6a4989c41b2cc415ec509
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-20 17:19:48 -0400

    Towards testing good and bad server setups

--- build_run_test.sh
+++ build_run_test.sh
@@ -13,10 +13,19 @@ cp -a input_fixtures $DATA_DIR
 # The on_startup script writes to the shared volume,
 # so we copy first to avoid contamination of the fixture.
 
+export GOOD_NAME=good_$NAME
+export BAD_NAME=bad_$NAME
+
 docker run --detach \
-           --name $NAME \
+           --name $GOOD_NAME \
            --publish 80 \
            --volume $DATA_DIR/good:/var/www/data \
            $NAME
 
-python test.py $NAME
+docker run --detach \
+           --name $BAD_NAME \
+           --publish 80 \
+           --volume $DATA_DIR/bad_assembly:/var/www/data \
+           $NAME
+
+python test.py
--- test.py
+++ test.py
@@ -9,38 +9,35 @@ class ContainerTest(unittest.TestCase):
     def setUp(self):
         # self.suffix = os.environ['SUFFIX']
         # self.stamp = os.environ['STAMP']
-        command = "docker port {NAME} | perl -pne 's/.*://'".format(**os.environ)
-        os.environ['PORT'] = subprocess.check_output(command, shell=True).strip().decode('utf-8')
-        url='http://localhost:{PORT}/'.format(**os.environ)
+        self.good_url = self.get_url(os.environ['GOOD_NAME'])
+        self.bad_url = self.get_url(os.environ['BAD_NAME'])
+
+    def tearDown(self):
+        print('good url: ' + self.good_url)
+
+    def get_url(self, name):
+        command = "docker port {} | perl -pne 's/.*://'".format(name)
+        port = subprocess.check_output(command, shell=True).strip().decode('utf-8')
+        url = 'http://localhost:{}/'.format(port)
         for i in xrange(5):
-            if 0 == subprocess.call('curl --fail --silent '+url+' > /dev/null', shell=True):
-                return
+            if 0 == subprocess.call('curl --fail --silent ' + url + ' > /dev/null', shell=True):
+                return url
             print('Still waiting for server...')
             time.sleep(1)
-        self.fail('Server never came up')
+        self.fail('Server never came up: ' + name)
 
     def test_home_page(self):
-        response = requests.get('http://localhost:{PORT}'.format(**os.environ))
+        response = requests.get(self.good_url)
         self.assertEqual(response.status_code, 200)
         self.assertRegexpMatches(response.text, r'>IGV<')
 
     def test_data_directory(self):
-        response = requests.get(
-            'http://localhost:{PORT}/data/input.json'.format(**os.environ)
-        )
+        response = requests.get(self.good_url + 'data/input.json')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
-    os.environ['NAME'] = sys.argv[1]
-
     suite = unittest.TestLoader().loadTestsFromTestCase(ContainerTest)
     result = unittest.TextTestRunner(verbosity=2).run(suite)
-    print(
-        (
-            'browse:   http://localhost:{PORT}/\n'
-            'clean up: docker stop docker_igv_js | xargs docker rm'
-        ).format(**os.environ)
-    )
     if result.wasSuccessful():
         print('PASS!')
     else:
